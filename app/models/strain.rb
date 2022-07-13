bc0f168ccc84258872bc70fd87e9bf80dc474dd3
Mario Munoz <mariog.munoztellez@gmail.com>
2022-07-13 16:01:14 -0400

    commitc2a

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -1,3 +1,5 @@
 class Strain < ApplicationRecord
     validates :name, presence: true, uniqueness: true
+    has_many :assemblies
+    has_many :wines, through: :assemblies
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,3 +1,5 @@
 class Wine < ApplicationRecord
     validates :name, presence: true, uniqueness: true
+    has_many :assemblies
+    has_many :strains, through: :assemblies
 end
